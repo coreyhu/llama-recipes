b5c5a412eacfdb6457e4856aaa790b9d192ea11b
corey hu <corey@coreyhu.com>
2023-11-02 04:52:56 +0000

    fmt

--- src/llama_recipes/configs/training.py
+++ src/llama_recipes/configs/training.py
@@ -35,7 +35,3 @@ class TrainConfig:
     dist_checkpoint_folder: str="fine-tuned" # will be used if using FSDP
     save_optimizer: bool=False # will be used if using FSDP
     use_fast_kernels: bool = False # Enable using SDPA from PyTroch Accelerated Transformers, make use Flash Attention and Xformer memory-efficient kernels
-
-    
-    
-    
